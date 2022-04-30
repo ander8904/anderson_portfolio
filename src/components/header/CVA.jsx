017ad623a46d616936e4b0e44b20f4f8da3eb127
ander8904 <andercamachobre@gmail.com>
2022-04-29 21:03:55 -0600

    fixing boolean hamnburguer

--- src/components/header/CVA.jsx
+++ src/components/header/CVA.jsx
@@ -10,7 +10,7 @@ export const CVA = () => {
                 <div class="rounded-md shadow">
                   <a href={CVF} download class="btn w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white  md:py-4 md:text-lg md:px-10"> Descargar Currículum </a>
                 </div>
-                <div class="mt-3 sm:mt-0 sm:ml-3">
+                <div class="mt-3 sm:mt-0 sm:ml-3 ">
                   <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"> Hablemos... </a>
                 </div>
               </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,17 +4,18 @@ import Spline from '@splinetool/react-spline';
 import { CVA } from './CVA'
 import { Spinner } from './Spinner';
 import { Nav } from './nav/Nav'
+import { Contact } from '../contact/Contact';
 
 export const Header = () => {
   return (
 
     <div className='relative bg-slate-800 overflow-hidden '>
-      <Nav />
-      <div class=" max-w-7xl mx-auto p-4 mt-20 ">
-
-        <main class=" mt-96 mx-auto  sm:mt-12 sm:px-6  lg:px-8 xl:mt-28">
+      <Nav/>
+      <div class=" max-w-7xl mx-auto p-4 md:mt-44  ">
+   
+        <section class=" mt-96 mx-auto sm:mt-12 sm:px-6  lg:px-8 xl:mt-28">
           <div class="sm:text-center lg:text-left">
-            <h1 class="text-4xl tracking-tight font-extrabold sm:text-5xl md:text-6xl">
+            <h1 class="text-4xl tracking-tight font-extrabold sm:text-5xl md:text-6xl  ">
               <span class="block snap-x:inline">Bienvenido, mi nombre es</span>
               <span class="block text-indigo-600 xl:inline">Anderson Camacho Brenes</span>
               <h5 className='block text-light'>Junior developer</h5>
@@ -24,15 +25,15 @@ export const Header = () => {
 
           </div>
 
-        </main>
+        </section>
       
-        <div class="lg:absolute lg:inset-y-24 lg:right-0 lg:w-1/2 ">
-          <Spline class='h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full' scene="https://draft.spline.design/NPo5k49tfh2iXsbA/scene.spline" />
+        <div class="lg:absolute lg:inset-y-24 lg:right-0 ">
+          <Spline scene="https://draft.spline.design/NPo5k49tfh2iXsbA/scene.spline" />
 
         </div>
       
       <br></br>
-
+      <Contact/>
       <br></br>
       <br></br>
       <br></br>
@@ -61,30 +62,7 @@ export const Header = () => {
       <br></br>
       <br></br>
       <br></br>
-      <Spinner/>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Spinner/>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Spinner/>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+    
     </div>
   )
 }
